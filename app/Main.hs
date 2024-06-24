eed1ffb01f65a33478f6f3d878afce702f75c7dc
Johnny Cheng <johnny.cheng7b9@gmail.com>
2024-06-24 15:48:17 -0700

    type signatures

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,40 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn (wrapHtml myhtml) 
+
+-- End 06-25-24: Embedded Domain-Specific Languages
+
+
+wrapHtml :: [Char] -> [Char]
+wrapHtml content = "<html><body>" <> content <> "</body></html>"
+
+myhtml :: [Char]
+myhtml = "Hello, workd"
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+
+el :: String -> String -> String
+el tag content = 
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
--- hello.hs
+++ hello.hs
@@ -5,3 +5,24 @@ main = putStrLn (wrapHtml myhtml)
 
 wrapHtml content = "<html><body>" <> content <> "</body></html>"
 myhtml = "Hello, workd"
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+
+el :: String -> String -> String
+el tag content = 
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
